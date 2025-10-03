a15da38afd2fe06a5153ae4fa0728e5d09dd183a
lisenchen168 <lisenchen168@gmail.com>
2025-10-03 20:24:19 +0800

    Update xhamster (2).js

--- xhamster (2).js
+++ xhamster (2).js
@@ -5,7 +5,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 let appConfig = {
     ver: 1,
     title: 'xhamster',
-    site: 'https://zh.xhamster.com',
+    site: 'https://www.xhamster.com',
     tabs: [
         {
             name: 'newest',
